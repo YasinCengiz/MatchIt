60924699e011fea8702bb2eb75dd0d9ff1b5764e
Yasin <mryasincengiz@gmail.com>
2022-02-09 21:55:27 +0400

    Initial Commit

---
+++ MatchIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MatchIt
+//
+//  Created by Yasin on 9.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatchIt/MatchItApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchItApp.swift
+//  MatchIt
+//
+//  Created by Yasin on 9.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MatchItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
